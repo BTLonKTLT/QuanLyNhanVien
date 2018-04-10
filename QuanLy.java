ab1ca32f3368ae66992f40e1929b028548486922
chison1997 <32763504+chison1997@users.noreply.github.com>
2018-04-10 18:12:43 +0700

    main()
    - Menu chọn

--- QuanLy.java
+++ QuanLy.java
@@ -1,5 +1,9 @@
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Date;
 import java.util.LinkedList;
+import java.util.Scanner;
 
 public class QuanLy {
     LinkedList<DonVi> list;
@@ -41,5 +45,50 @@ public class QuanLy {
     public static void main(String [] args){
         //switch case -> chọn 1 trong các chức năng
         
+    	Scanner input = new Scanner(System.in);
+		String choice;
+		while(true)
+		{
+			System.out.println("Quản lý nhân sự BK Corporation");
+			System.out.println("---------------------------------");
+			System.out.println("1. Thông tin cơ bản");
+			System.out.println("2. Tìm kiếm thông tin nhân viên");
+			System.out.println("3. Hiển thị tình trạng làm việc của nhân viên");
+			System.out.println("4. Hiển thị tình trạng của một đơn vị");
+			System.out.println("5. Thêm nhân viên mới");
+			System.out.println("6. Cập nhật thông tin nhân viên");
+			System.out.println("7. Thoát");
+			System.out.print("Chọn 1 trong các chức năng sau: "); 
+			choice = input.nextLine();
+			
+			switch(choice.charAt(0))
+			{
+			case '1': //Thông tin cơ bản
+				
+				break;
+			case '2': //Tìm kiếm thông tin nhân viên
+				
+	
+				break;
+			case '3': //Hiển thị tình trạng làm việc của nhân viên
+				
+				break;
+			case '4': //Hiển thị tình trạng của một đơn vị
+				
+				break;
+			case '5': //Thêm nhân viên mới
+				
+				break;
+			case '6': //Cập nhật thông tin nhân viên
+				
+				break;
+			case '7': //Thoát
+				
+				break;
+			
+				
+			}
+			choice = input.nextLine();
+		
     }
 }
