3f1dfba8622d58064d74d232a88ad6d7b80fc059
chison1997 <32763504+chison1997@users.noreply.github.com>
2018-04-16 20:21:34 +0700

    create data
    - Fix
    - Email dùng static char thay vì cấp phát động
    - Sinh mã nhân viên từ 4-100
    - Khoảng thời gian đến, về ngắn hơn, hợp lý hơn
    - Dùng sizeof để tính số phần tử các mảng dữ liệu sinh thay vì hằng số

--- createData.cpp
+++ createData.cpp
@@ -7,8 +7,8 @@ int ngay, thang, nam;
 
 char* createEmail(char* HD, char* T)
 {
-	char* email = new char[30];
-	int index;
+	static char email[64];
+	int index = 0;
 	for(int i = 0; i < strlen(HD); i++)
 	{
 		if (HD[i] != ' ') 
@@ -25,6 +25,7 @@ char* createEmail(char* HD, char* T)
 	}
 	
 	email[index] = '\0';
+
 	return email;
 }
 
@@ -91,25 +92,28 @@ main()
 	char* pho[] = {"Ta Quang Buu", "Tran Dai Nghia", "Le Thanh Nghi", "Tran Hung Dao", "Tay Son", "Thai Ha", "Thanh Xuan", "Quang Trung", "Nguyen Trai", "Hai Ba Trung"};
 	char* dauSDT[] = {"098", "016", "097", "012", "088"};
 	
-	char* hour[] = {"06", "07", "08", "17", "18"};
-	char* minute[] = {"00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59"};
-	char* day[] = {"00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"};
-	char* month[] = {"00", "01", "02", "03", "04", "05","06","07","08","09","10","11","12"};
+	//char* hour[] = {"06", "07", "08", "17", "18"};
+//	char* minute[] = {"00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59"};
+//	char* day[] = {"00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"};
+//	char* month[] = {"00", "01", "02", "03", "04", "05","06","07","08","09","10","11","12"};
 	
 	FILE* f = fopen("ThongTinNhanVien.txt", "w");
-	for(int i = 1; i <= 100; i++)
+
+
+	//Mã nhân viên từ 4 đến 100
+	for(int i = 4; i <= 100; i++) 
 	{
 		fprintf(f, "NV%d\n", i);
-		char* HD = hoDem[rand() % 15];
+		char* HD = hoDem[rand() % (sizeof(hoDem) / sizeof(char*))];
 		fprintf(f, "%s\n", HD);
-		char* T = ten[rand() % 21];
+		char* T = ten[rand() % (sizeof(ten) / sizeof(char*))];
 		fprintf(f, "%s\n", T);
-		fprintf(f, "%s - %s\n", phong[rand() % 5], congTy[rand() % 3]);
+		fprintf(f, "%s - %s\n", phong[rand() % (sizeof(phong) / sizeof(char*))], congTy[rand() % (sizeof(congTy) / sizeof(char*))]);
 		fprintf(f, "%d/%d/%d\n", rand() % 28 + 1, rand() % 12 +1, rand() % 40 + 1955);
-		fprintf(f, "%s\n", tinhThanh[rand() % 10]);
-		fprintf(f, "So %d, %s\n", rand() % 100 + 1, pho[rand() % 10]);
+		fprintf(f, "%s\n", tinhThanh[rand() % (sizeof(tinhThanh) / sizeof(char*))]);
+		fprintf(f, "So %d, %s\n", rand() % 100 + 1, pho[rand() % (sizeof(pho) / sizeof(char*))]);
 		fprintf(f, "%s@gmail.com\n", createEmail(HD, T));
-		fprintf(f,"%s%d%d%d%d%d%d%d\n", dauSDT[rand() % 5], rand()%10, rand()%10, rand()%10, rand()%10, rand()%10, rand()%10, rand()%10);
+		fprintf(f,"%s%d%d%d%d%d%d%d\n", dauSDT[rand() % (sizeof(dauSDT) / sizeof(char*))], rand()%10, rand()%10, rand()%10, rand()%10, rand()%10, rand()%10, rand()%10);
 		ngay = rand() % 28 + 1;
 		thang = rand() % 12 + 1;
 		nam = rand() % 7 + 2010;
@@ -124,7 +128,7 @@ main()
 					continue;
 			if (rand() % 20 == 0) continue;
 			if (check())
-				fprintf(f, "%s/%s/%d, %s:%s, %s:%s\n", day[ngay],month[thang], nam, hour[rand() % 3], minute[rand() % 60], hour[rand() % 2 + 3], minute[rand() % 60] );
+				fprintf(f, "%02d/%02d/%d, %02d:%02d, %02d:%02d\n", ngay, thang, nam, rand() % 2 + 8, rand() % 60, rand() % 2 + 16, rand() % 60 );
 													/*	day[ngay],month[thang], nam*/
 			else 
 				break;
